67589446a15c6dee33cd13e4888aed98d0676f76
Yura Push <youdeimos1@gmail.com>
2023-02-06 14:23:47 +0200

    Reversed number

--- reverse_number.py
+++ reverse_number.py
@@ -1,5 +1,9 @@
 num1 = 186
+num2 = 867
+num3 = 374
 
 str_num1 = str(num1)
-rev_num1 = str_num1[2] + str_num1[1] + str_num1[0]
-print(int(rev_num1))
+rev_num1 = int(str_num1[2] + str_num1[1] + str_num1[0])
+
+
+print("186 reversed - ", rev_num1)
