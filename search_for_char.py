2709150df17e996ae915cf99d5fe34d6c3f1037e
Yura Push <youdeimos1@gmail.com>
2023-02-12 18:55:11 +0200

    find an index

--- search_for_char.py
+++ search_for_char.py
@@ -1,2 +1,9 @@
 s = input("Enter a string:")
 ch = input("Enter a character")
+
+i = -1
+while True:
+    i = s.find(ch, i+1)
+    if i == -1:
+        break
+    print("Indexes of letters:", i, end=' ')
