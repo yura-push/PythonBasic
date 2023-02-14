a2189c28c56d4d1e9abd3a4fb9e1e2d3b8969d1a
Yura Push <youdeimos1@gmail.com>
2023-02-14 18:14:36 +0200

    bigger than neighbors

--- neighbors.py
+++ neighbors.py
@@ -5,10 +5,9 @@ for i in range(1, 10):
     list_of_numbers.append(random.randint(1,10))
 print(list_of_numbers)
 
-i = 1
 counter = 0
-for i in range(len(list_of_numbers) - 1):
-    if list_of_numbers[i] > list_of_numbers[i+1] and list_of_numbers[i] > list_of_numbers[i-1]:
+for i in range(1, len(list_of_numbers[:-1])):
+    if list_of_numbers[i] > list_of_numbers[i-1] and list_of_numbers[i] > list_of_numbers[i+1]:
         counter += 1
 
 print("Number of values bigger than it`s neighbors:", counter)
