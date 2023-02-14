dac42a3afa9917828556ba14e8ed9721f0adef56
Yura Push <youdeimos1@gmail.com>
2023-02-14 19:21:39 +0200

    bigger than neighbors

--- neighbors.py
+++ neighbors.py
@@ -2,7 +2,7 @@ import random
 
 list_of_numbers = []
 for i in range(1, 10):
-    list_of_numbers.append(random.randint(1,10))
+    list_of_numbers.append(random.randint(1, 10))
 print(list_of_numbers)
 
 counter = 0
