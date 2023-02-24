a3e013751043b84fb69c5e07f06fbbff14c822dd
Yura Push <youdeimos1@gmail.com>
2023-02-24 15:37:24 +0200

    accounting book

--- accounting_book.py
+++ accounting_book.py
@@ -11,3 +11,9 @@ def total_price(*lists):
     return list(zip([i[0] for i in lists], [i[2] * i[3] if i[2] * i[3] > 100 else i[2] * i[3] + 10 for i in lists]))
 
 print("order number : total_price", total_price(*lists))
+
+
+my_lambda = lambda list_var: (list_var[0], list_var[2] * list_var[3]) if list_var[2] * list_var[3] > 100 \
+    else (list_var[0], list_var[2] * list_var[3] + 10)
+result = list(map(my_lambda, lists))
+print(result)
