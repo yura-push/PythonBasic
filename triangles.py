d14b98104809b5e4e17fef46fe04aa7b5c69dd70
Yura Push <youdeimos1@gmail.com>
2023-02-26 16:02:58 +0200

    patterns

--- triangles.py
+++ triangles.py
@@ -2,13 +2,21 @@ n = int(input("Enter the size of triangle: "))
 
 # Picture A
 for i in range(n):
-    for j in range(n):
+    for j in range(i, n):
         print(" ", end=" ")
     for j in range(i):
-        print("*", end=" ")
-    for j in range(i + 1):
-        print("*", end=" ")
+        if i == n-1 or j == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    for j in range(i+1):
+        if i == n - 1 or j == i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
     print()
+
+print()
 
 # Picture B
 for i in range(n):
@@ -16,10 +24,28 @@ for i in range(n):
         print(" ", end=" ")
     for j in range(i):
         print("*", end=" ")
+
     for j in range(i + 1):
         print("*", end=" ")
     print()
 
+
 print()
 
-#
+# Picture C
+for i in range(n-1):
+    for j in range(i, n-1):
+        print(" ", end=" ")
+    for j in range(i):
+        print("*", end=" ")
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ", end=" ")
+    for j in range(i, n):
+        print("*", end=" ")
+    for j in range(i, n-1):
+        print("*", end=" ")
+    print()
