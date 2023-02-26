c0a5a223b448a752ada54e47846eadf929d23396
Yura Push <youdeimos1@gmail.com>
2023-02-26 15:40:01 +0200

    patterns

--- triangles.py
+++ triangles.py
@@ -1,10 +1,25 @@
-height = int(input("Enter the heights of triangle: "))
+n = int(input("Enter the size of triangle: "))
 
-for i in range(height):
-    for j in range(height - i):
+# Picture A
+for i in range(n):
+    for j in range(n):
         print(" ", end=" ")
     for j in range(i):
         print("*", end=" ")
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+
+# Picture B
+for i in range(n):
+    for j in range(i, n):
+        print(" ", end=" ")
     for j in range(i):
         print("*", end=" ")
-    print("*")
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+
+print()
+
+#
