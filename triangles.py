ee8ba7bfb0daef2db12562f8329ac1f0021a61c6
Yura Push <youdeimos1@gmail.com>
2023-02-27 17:19:17 +0200

    patterns

--- triangles.py
+++ triangles.py
@@ -5,12 +5,12 @@ for i in range(n):
     for j in range(i, n):
         print(" ", end=" ")
     for j in range(i):
-        if i == n-1 or j == 0:
+        if i == n-1 or j ==0:
             print("*", end=" ")
         else:
             print(" ", end=" ")
     for j in range(i+1):
-        if i == n - 1 or j == i:
+        if i == n-1 or i == j:
             print("*", end=" ")
         else:
             print(" ", end=" ")
@@ -24,17 +24,38 @@ for i in range(n):
         print(" ", end=" ")
     for j in range(i):
         print("*", end=" ")
-
-    for j in range(i + 1):
+    for j in range(i+1):
         print("*", end=" ")
     print()
-
-
-print()
 
 # Picture C
 for i in range(n-1):
     for j in range(i, n-1):
+        print(" ", end=" ")
+    for j in range(i):
+        print("*", end=" ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ", end=" ")
+    for j in range(i, n-1):
+        if i==0 or i == j:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    for j in range(i, n):
+        if i==0 or j == n-1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+print()
+# Picture D
+for i in range(n - 1):
+    for j in range(i, n - 1):
         print(" ", end=" ")
     for j in range(i):
         print("*", end=" ")
@@ -44,8 +65,14 @@ for i in range(n-1):
 for i in range(n):
     for j in range(i):
         print(" ", end=" ")
+    for j in range(i, n - 1):
+        if i == 0 or i == j:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
     for j in range(i, n):
-        print("*", end=" ")
-    for j in range(i, n-1):
-        print("*", end=" ")
+        if i == 0 or j ==i or j == n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
     print()
