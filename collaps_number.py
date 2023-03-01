37687a785a1c77668d1fb74d1361bdf2af6789e2
Yura Push <youdeimos1@gmail.com>
2023-03-01 19:56:14 +0200

    collapse numbers

--- collaps_number.py
+++ collaps_number.py
@@ -1,10 +1,14 @@
 def number_collaps(num):
-    list_num = list(map(int, str(num)))
-    total_sum = sum(list_num)
-    # print(temp_sum)
-    while len(str(total_sum)) >= 2:
-        total_sum_list = list(map(int, str(total_sum)))
-        total_sum = sum(total_sum_list)
-    return total_sum
+    try:
+        list_num = list(map(int, str(num)))
+        total_sum = sum(list_num)
+        # print(temp_sum)
+        while len(str(total_sum)) >= 2:
+            total_sum_list = list(map(int, str(total_sum)))
+            total_sum = sum(total_sum_list)
+        return total_sum
+    except ValueError as exc:
+        print("Only positive integer values!")
 
-print(number_collaps(78))
+
+print(number_collaps(8938))
