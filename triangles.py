144a39846ad2d0c0ccc5299396b87f3cda92f10a
Yura Push <youdeimos1@gmail.com>
2023-03-01 14:17:25 +0200

    triangles

--- triangles.py
+++ triangles.py
@@ -1,4 +1,4 @@
-def print_hollow_triangle(height, fill_symbol=".", outer_symbol=".", border_symbol="*"):
+def print_triangle(height, fill_symbol=".", outer_symbol=".", border_symbol="*"):
     for i in range(height - 1):
         print(
             outer_symbol * (height - i - 1),
@@ -12,9 +12,11 @@ def print_hollow_triangle(height, fill_symbol=".", outer_symbol=".", border_symb
     print(border_symbol * (height * 2 - 1))
 
 
-print_hollow_triangle(8, fill_symbol=" ", outer_symbol=" ")
+#print_triangle(8, fill_symbol="*", outer_symbol=" ")
+#print()
 
-def print_triangle(height, fill_symbol=".", outer_symbol=".", border_symbol="*"):
+
+def print_diamond(height, fill_symbol=".", outer_symbol=".", border_symbol="*"):
     for i in range(height - 1):
         print(
             outer_symbol * (height - i - 1),
@@ -27,4 +29,41 @@ def print_triangle(height, fill_symbol=".", outer_symbol=".", border_symbol="*")
         )
     print(border_symbol * (height * 2 - 1))
 
-print_triangle(8, fill_symbol="*", outer_symbol=" ")
+    for i in range(height-2, -1, -1):
+        print(
+            outer_symbol * (height - i - 1),
+            border_symbol,
+            outer_symbol * (2 * i - 1),
+            border_symbol if i > 0 else "",
+            outer_symbol * (height - i - 1),
+            sep=""
+        )
+
+
+def print_diamond2(height, fill_symbol=".", outer_symbol=".", border_symbol="*"):
+    for i in range(height - 1):
+        print(
+            outer_symbol * (height - i - 1),
+            border_symbol,
+            fill_symbol * (i - 1),
+            fill_symbol * i,
+            border_symbol if i > 0 else "",
+            outer_symbol * (height - i - 1),
+            sep=""
+        )
+    print(border_symbol * (height * 2 - 1))
+
+    for i in range(height-2, -1, -1):
+        print(
+            outer_symbol * (height - i - 1),
+            border_symbol,
+            outer_symbol * (i - 1),
+            border_symbol if i > 0 else "",
+            outer_symbol * (i - 1),
+            border_symbol if i > 0 else " ",
+            outer_symbol * (height - i - 1),
+            sep=""
+        )
+
+
+print_diamond2(8, fill_symbol="*", outer_symbol=" ")
