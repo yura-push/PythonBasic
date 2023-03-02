93c9ce0f353f46f0889b610675c51c9642a598be
Yura Push <youdeimos1@gmail.com>
2023-03-02 19:51:53 +0200

    expand number

--- expanded_number.py
+++ expanded_number.py
@@ -9,6 +9,7 @@ def expanded_form(num: int) -> str:
 
 def expanded_form1(num):
     list_num = [int(i) for i in str(num)]
-    return " + ".join([str(list_num[i]) + "0"*(len(list_num) - 1 - i) for i in range(len(list_num)) if list_num[i] > 0])
+    return " + ".join(
+        [str(list_num[i]) + "0" * (len(list_num) - 1 - i) for i in range(len(list_num)) if list_num[i] > 0])
 
 #print(expanded_form1(70304))
