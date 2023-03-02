affcaf55df729d8d12b74f650d4fd4cd9171eab1
Yura Push <youdeimos1@gmail.com>
2023-03-02 19:53:56 +0200

    expand number

--- expanded_number.py
+++ expanded_number.py
@@ -8,8 +8,11 @@ def expanded_form(num: int) -> str:
 #print(expanded_form(70304))
 
 def expanded_form1(num):
-    list_num = [int(i) for i in str(num)]
-    return " + ".join(
-        [str(list_num[i]) + "0" * (len(list_num) - 1 - i) for i in range(len(list_num)) if list_num[i] > 0])
+    try:
+        list_num = [int(i) for i in str(num)]
+        return " + ".join(
+            [str(list_num[i]) + "0" * (len(list_num) - 1 - i) for i in range(len(list_num)) if list_num[i] > 0])
+    except ValueError as exc:
+        print("Only positive integer values!")
 
-#print(expanded_form1(70304))
+print(expanded_form1())
