e8fd461ea7f4a4b4fc9898f2c6014d9c1927ef9b
Yura Push <youdeimos1@gmail.com>
2023-03-02 18:40:44 +0200

    expand number

--- expanded_number.py
+++ expanded_number.py
@@ -1,7 +1,13 @@
 def expanded_form(num: int) -> str:
-    string_num = str(num)
+    list_num = [i for i in num]
 
 
-num = 12
-print(list(map(int, str(num))))
-print(expanded_form(1085))
+num = 120503
+list_num = [int(i) for i in str(num)]
+
+#for i in range(len(list_num)):
+    #print(str(list_num[i]), end="")
+
+for i in range(len(list_num)):
+    if list_num[i] > 0:
+        print(str(list_num[i]) + "0"*(len(list_num) - 1 - i) + " + " if i < len(list_num) else "", end="")
