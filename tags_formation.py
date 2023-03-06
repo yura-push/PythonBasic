1f0948bedf870d72ee2b1133049dafc9fde6b70d
Yura Push <youdeimos1@gmail.com>
2023-03-06 20:33:24 +0200

    tags formation

--- tags_formation.py
+++ tags_formation.py
@@ -1,8 +1,8 @@
-def hash_tag_creator2(string_var: str) -> str:
+def hash_tag_creator(string_var: str) -> str:
     if len(string_var) <= 0:
         raise ValueError("empty string")
 
     return "#" + (string_var.title()).replace(" ", "") if len(string_var) < 140 else False
 
 
-print(hash_tag_creator2(""))
+print(hash_tag_creator(""))
