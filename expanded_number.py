c1e5675e87809ec97802fd153eca7eb53bb1cf44
Yura Push <youdeimos1@gmail.com>
2023-03-06 19:29:21 +0200

    collapse number

--- expanded_number.py
+++ expanded_number.py
@@ -1,10 +1,10 @@
-def expanded_form(num):
-    try:
-        list_num = [int(i) for i in str(num)]
-        return "'" + " + ".join(
-            [str(list_num[i]) + "0" * (len(list_num) - 1 - i) for i in range(len(list_num)) if list_num[i] > 0]) + "'"
-    except ValueError as exc:
-        print(exc, "only positive integer values")
+def expanded_form2(num):
+    if num <= 0:
+        raise Exception("only integers bigger than 0")
+
+    list_num = [int(i) for i in str(num)]
+    return "'" + " + ".join(
+        [str(list_num[i]) + "0" * (len(list_num) - 1 - i) for i in range(len(list_num)) if list_num[i] > 0]) + "'"
 
 
-print(expanded_form(70305))
+print(expanded_form2(73045))
