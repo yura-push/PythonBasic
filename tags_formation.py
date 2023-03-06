b5483027a3fae74c4bc83f127bed69156049a380
Yura Push <youdeimos1@gmail.com>
2023-03-06 19:26:13 +0200

    tags formation

--- tags_formation.py
+++ tags_formation.py
@@ -1,12 +1,8 @@
-def hash_tag_creator(string_var: str) -> str:
-    try:
-        hash_tag_var = "#" + (string_var.title()).replace(" ", "") if len(string_var) < 140 else False
-        if len(string_var) == 0 or len(hash_tag_var) == 0:
-            raise ValueError("empty string")
-        else:
-            return hash_tag_var
-    except ValueError as exc:
-        print(exc)
+def hash_tag_creator2(string_var: str) -> str:
+    if len(string_var) <= 0:
+        raise ValueError("empty string")
+
+    return "#" + (string_var.title()).replace(" ", "") if len(string_var) < 140 else False
 
 
-print(hash_tag_creator(" hash tag   creator  "))
+print(hash_tag_creator2(""))
