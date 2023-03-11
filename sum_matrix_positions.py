ff574e061d1703f39dd1fcb72c358a91fb94c276
Yura Push <youdeimos1@gmail.com>
2023-03-11 14:41:08 +0200

    sum matrix rows and columns

---
+++ sum_matrix_positions.py
@@ -0,0 +1,24 @@
+from random import randint
+from pprint import pprint
+
+M = int(input("Enter number of rows: "))
+N = int(input("Enter number of columns: "))
+
+matrix = [[randint(1, 50) for j in range(N)] for i in range(M)]
+
+
+def sum_matrix_positions(m):
+    for i in range(M):
+        sum_row = 0
+        for j in range(N):
+            sum_row += matrix[i][j]
+        print("\t".join(map(str, matrix[i])), "\t",  sum_row)
+
+    for j in range(N):
+        sum_column = 0
+        for i in range(len(matrix)):
+            sum_column += matrix[i][j]
+        print(sum_column, end="\t")
+
+
+sum_matrix_positions(matrix)
