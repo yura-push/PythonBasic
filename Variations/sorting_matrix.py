f4a683cb0b3db2ed77b79c710bc66473face66fb
Yura Push <youdeimos1@gmail.com>
2023-03-14 20:59:31 +0200

    sorting matrix using selection sort

---
+++ Variations/sorting_matrix.py
@@ -0,0 +1,73 @@
+from random import randint
+from pprint import pprint
+
+M = int(input("Введіть розмір матриці: "))
+matrix = [[randint(1, 50) for j in range(M)] for i in range(M)]
+
+
+def sort_matrix(matrix):
+    # знаходимо кількість рядків та стовпців у матриці
+    m = len(matrix)
+    n = len(matrix[0])
+
+    # знаходимо суму елементів кожного стовпця
+    column_sums = [0] * n
+    for j in range(n):
+        for i in range(m):
+            column_sums[j] += matrix[i][j]
+
+    # сортуємо стовпці за зростанням сум елементів
+    for j in range(n - 1):
+        min_sum = column_sums[j]
+        min_index = j
+        for k in range(j + 1, n):
+            if column_sums[k] < min_sum:
+                min_sum = column_sums[k]
+                min_index = k
+        if min_index != j:
+            # міняємо місцями стовпці
+            for i in range(m):
+                matrix[i][j], matrix[i][min_index] = matrix[i][min_index], matrix[i][j]
+            column_sums[j], column_sums[min_index] = column_sums[min_index], column_sums[j]
+
+    # сортуємо кожен непарний стовпець
+    for j in range(1, n, 2):
+        for i in range(m - 1):
+            min_value = matrix[i][j]
+            min_index = i
+            for k in range(i + 1, m):
+                if matrix[k][j] < min_value:
+                    min_value = matrix[k][j]
+                    min_index = k
+            if min_index != i:
+                # міняємо місцями елементи
+                matrix[i][j], matrix[min_index][j] = matrix[min_index][j], matrix[i][j]
+
+    # сортуємо кожен парний стовпець
+    for j in range(0, n, 2):
+        for i in range(m - 1):
+            max_value = matrix[i][j]
+            max_index = i
+            for k in range(i + 1, m):
+                if matrix[k][j] > max_value:
+                    max_value = matrix[k][j]
+                    max_index = k
+            if max_index != i:
+                # міняємо місцями елементи
+                matrix[i][j], matrix[max_index][j] = matrix[max_index][j], matrix[i][j]
+
+    return matrix, column_sums
+
+
+def print_sorted_matrix(matrix, col_sums):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            print(matrix[i][j], end="\t")
+        print()
+
+    for i in range(len(col_sums)):
+        print(col_sums[i], end="\t")
+
+
+sorted_matrix, column_sums = sort_matrix(matrix)
+print_sorted_matrix(sorted_matrix, column_sums)
--- sorting_matrix.py
+++ sorting_matrix.py
@@ -4,22 +4,22 @@ from pprint import pprint
 m = int(input("Enter the size of matrix (bigger than 5): "))
 
 matrix = [[random.randint(1, 50) for i in range(m)] for j in range(m)]
-pprint(matrix)
+columns_sum = [0] * m
 
-# t_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-# for i in range(len(t_matrix)):
-#     sum_row = 0
-#     for j in range(len(t_matrix[0])):
-#         sum_row += t_matrix[i][j]
-#     t_matrix.append(sum_row)
-#     print(t_matrix[i], sum_row)
+#pprint(matrix)
 
+for j in range(len(matrix)):
+    col_sum = 0
+    for i in range(len(matrix)):
+        col_sum += matrix[j][i]
+    columns_sum[j] = col_sum
+matrix.append(columns_sum)
 
-answer = []
-for i in range(len(matrix)):
-    sum_col = 0
-    for j in range(len(matrix[0])):
-        sum_col += matrix[j][i]
-    answer.append(sum_col)
-matrix.append(answer)
+for i in range(len(matrix) - 1):
+    for j in range(len(matrix) - i - 1):
+        if columns_sum[j] > columns_sum[j+1]:
+            columns_sum[j], columns_sum[j + 1] = columns_sum[j + 1], columns_sum[j]
+            for k in range(len(matrix)):
+                matrix[k][j], matrix[k][j+1] = matrix[k][j+1], matrix[k][j]
+
 pprint(matrix)
