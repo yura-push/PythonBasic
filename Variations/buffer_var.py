3eb43a2f610771bddf25b639343f44d897723be1
Yura Push <youdeimos1@gmail.com>
2023-03-24 14:41:29 +0200

    buffer

--- Variations/buffer_var.py
+++ Variations/buffer_var.py
@@ -6,15 +6,11 @@ class Buffer:
     def add(self, *a):
         for num in a:
             self.buffer.append(num)
-            if len(self.buffer) == 5:
-                self.sum_of_5 = sum(self.buffer[:5])
-                self.buffer = self.buffer[5:]
 
     def get_current_part(self):
-        if len(self.buffer) == 5:
-            print(self.sum_of_5)
-        else:
-            print(0)
+        if len(self.buffer) >= 5:
+            print(f"Sum of {self.buffer[:5]} elements = {sum(self.buffer[:5])}")
+            self.buffer = self.buffer[5:]
 
 
 if __name__ == "__main__":
