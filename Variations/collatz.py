82051301ac606b92019e450d715c47312940865d
Yura Push <youdeimos1@gmail.com>
2023-03-27 18:20:53 +0300

    collatz sequence

---
+++ Variations/collatz.py
@@ -0,0 +1,13 @@
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    if number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
+
+
+user_input = int(input("Enter a number: "))
+test_val = collatz(user_input)
+while test_val != 1:
+    test_val = collatz(test_val)
