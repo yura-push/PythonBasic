f0e3856069e1e3ffc657ad5d86e6cdb62a93cc41
Yura Push <youdeimos1@gmail.com>
2023-04-13 19:22:30 +0300

    displays a well-organized table with each column right-adjusted

--- Automate_boring_stuff/table_printer.py
+++ Automate_boring_stuff/table_printer.py
@@ -7,7 +7,6 @@ def print_table(table):
                 max_length = len(word)
         col_width.append(max_length)
 
-    right_width = max(col_width)
     for j in range(len(table[0])):
         for i in range(len(table)):
             print(table[i][j].rjust(max(col_width)), end="")
