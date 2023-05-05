e56b86f1906d1abaf6364782cc92a437d5f15444
Yura Push <youdeimos1@gmail.com>
2023-05-05 19:19:33 +0300

    this version copies all ".exe" files and creating folders in which they originally was

--- Automate_boring_stuff/Chapter_10_projects/selective_copy.py
+++ Automate_boring_stuff/Chapter_10_projects/selective_copy.py
@@ -9,11 +9,14 @@ import shutil
 directory = "C:\\D_Drive\\Python\\"
 destination = "C:\\D_Drive\\folder_copy2\\"
 
-for folder_name, sub_folders, filenames in os.walk(directory):
+for foldername, subfolders, filenames in os.walk(directory):
     for filename in filenames:
-        file_extension = ".exe"
-        if file_extension in filename:
-            source_file_path = os.path.join(folder_name, filename)
-            dest_file_path = os.path.join(destination, os.path.relpath(source_file_path,directory))
-            shutil.copy2(source_file_path, dest_file_path)
-            print(F"FILE {filename} COPIED TO {destination}")
+        if filename.endswith(".exe"):
+            source_file_path = os.path.join(foldername, filename)
+            dest_file_path = os.path.join(destination, os.path.relpath(source_file_path, directory))
+            # Checks if all directories exist in destination
+            # if not, creates folders and copies files.
+            if not os.path.exists(dest_file_path):
+                os.makedirs(dest_file_path)
+                shutil.copy2(source_file_path, dest_file_path)
+                print(F"FILE {filename} COPIED FROM {foldername} TO {dest_file_path}")
