ed7a692f2c43307ae995d57685c18d2fc3f275c9
Yura Push <youdeimos1@gmail.com>
2023-05-05 20:06:19 +0300

    this version copies all ".exe" files but it`s not checking if it already exists in dest fold and copies it 2 times

--- Automate_boring_stuff/Chapter_10_projects/selective_copy.py
+++ Automate_boring_stuff/Chapter_10_projects/selective_copy.py
@@ -6,17 +6,29 @@
 import os
 import shutil
 
-directory = "C:\\D_Drive\\Python\\"
-destination = "C:\\D_Drive\\folder_copy2\\"
+source_directory = "C:\\D_Drive\\Python\\"
+destination_directory = "C:\\D_Drive\\folder_copy2\\"
 
-for foldername, subfolders, filenames in os.walk(directory):
-    for filename in filenames:
-        if filename.endswith(".exe"):
-            source_file_path = os.path.join(foldername, filename)
-            dest_file_path = os.path.join(destination, os.path.relpath(source_file_path, directory))
-            # Checks if all directories exist in destination
-            # if not, creates folders and copies files.
-            if not os.path.exists(dest_file_path):
-                os.makedirs(dest_file_path)
-                shutil.copy2(source_file_path, dest_file_path)
-                print(F"FILE {filename} COPIED FROM {foldername} TO {dest_file_path}")
+# for foldername, subfolders, filenames in os.walk(directory):
+#     for filename in filenames:
+#         if filename.endswith(".exe"):
+#             source_file_path = os.path.join(foldername, filename)
+#             dest_file_path = os.path.join(destination, os.path.relpath(source_file_path, directory))
+#             # Checks if all directories exist in destination
+#             # if not, creates folders and copies files.
+#             if not os.path.exists(dest_file_path):
+#                 os.makedirs(dest_file_path)
+#                 shutil.copy2(source_file_path, dest_file_path)
+#                 print(F"FILE {filename} COPIED FROM {foldername} TO {dest_file_path}")
+
+if not os.path.exists(destination_directory):
+    os.makedirs(destination_directory)
+
+for root, dirs, files in os.walk(source_directory):
+    for file in files:
+        if file.endswith('.exe'):
+            source_file_path = os.path.join(root, file)
+            dest_file_path = os.path.join(destination_directory, file)
+            shutil.copy2(source_file_path, dest_file_path)
+            print(F"FILE {file} COPIED FROM {source_file_path} TO {destination_directory}")
+
